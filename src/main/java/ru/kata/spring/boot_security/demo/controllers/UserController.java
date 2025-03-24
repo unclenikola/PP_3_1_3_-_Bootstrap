9b507f2048d2c2d06d8434fcae51a9f5d436ffd2
unclenikola <9107066065@mail.ru>
2025-03-24 23:04:41 +0300

    ok

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -1,19 +1,22 @@
-//package ru.kata.spring.boot_security.demo.controllers;
-//
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import ru.kata.spring.boot_security.demo.model.User;
-//
-//@Controller
-//@RequestMapping("/user")
-//public class UserController {
-//
-//    @GetMapping
-//    public String userPage(@AuthenticationPrincipal User user, Model model) {
-//        model.addAttribute("user", user);
-//        return "user";
-//    }
-//}
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String userPage(@AuthenticationPrincipal User currentUser, Model model) {
+        if (currentUser == null) {
+            return "redirect:/login";
+        }
+        model.addAttribute("currentUser", currentUser);
+        return "user-page";
+    }
+}
